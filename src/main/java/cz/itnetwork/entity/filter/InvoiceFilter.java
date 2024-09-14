08b430dc1c7737a72b3df7c7011fcdf34b8f2542
KatGall <166738965+KatGall@users.noreply.github.com>
2024-09-14 15:09:05 +0200

    Vyčistění komentářů
    Vyčistění komentářů v invoice filter

--- src/main/java/cz/itnetwork/entity/filter/InvoiceFilter.java
+++ src/main/java/cz/itnetwork/entity/filter/InvoiceFilter.java
@@ -1,21 +1,13 @@
 package cz.itnetwork.entity.filter;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import cz.itnetwork.dto.PersonDTO;
 import lombok.Data;
-
-import java.time.LocalDate;
 
 @Data
 public class InvoiceFilter {
-
- //komentař
-    //komentář2 
     private Long buyerID;
     private Long sellerID;
     private String product;
     private Integer minPrice;
     private Integer maxPrice;
     private Integer limit = 10;
-
 }
